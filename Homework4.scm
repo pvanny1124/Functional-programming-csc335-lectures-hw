75add2f64ba3fd1c14790177d413e5d364d42d75
pvanny1124 <pvanny1124@gmail.com>
2018-10-30 17:51:50 -0400

    finish #2

--- Homework4.scm
+++ Homework4.scm
@@ -29,12 +29,34 @@
 
 ; The idea is suggested by (my-length '(a b c d)) = 4.  
 
+(define (my-length lst)
+    (cond ((null? lst) 0)
+            (else (+ 1 (my-length (cdr lst))))))
+
+(define (my-length lst)
+        (define (iter lst count)
+            (cond ((null? lst) count)
+                    (else (iter (cdr lst) (+ count 1)))))
+        (iter lst 0))
+
 
 ; 2.  Write your own version of list-ref using the list functions we have discussed.  You can find
 ; list-ref documented at http://www.schemers.org/Documents/Standards/R5RS/
 
 ; Briefly, the idea is indicated by this example:  (my-list-ref '(a b c d) 2) = c.  Note the 0-based
 ; indexing.  What happens if the input index exceeds the size of the input list?
+
+(define (my-list-ref lst num)
+        (cond ((zero? num) (car lst))
+                (else (my-list-ref (cdr lst) (- num 1)))
+))
+
+(define (my-list-ref lst num)
+        (cond ((zero? num) (car lst))
+                (else (car (cons (my-list-ref (cdr lst) (- num 1)) '())))
+        )
+)
+
 
 ; 3. Write a function start that takes two arguments, lst and num, and which returns the
 ; first num elements of lst.
