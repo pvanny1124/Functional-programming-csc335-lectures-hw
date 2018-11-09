2c55b6971556f8e5f3fda1029d75725d6d3b6e52
pvanny1124 <pvanny1124@gmail.com>
2018-11-09 18:06:10 -0500

    finish replace-nth

--- Homework6.scm
+++ Homework6.scm
@@ -22,11 +22,43 @@
 ; lst by new (and leave everything else unchanged)
 
 (define (replace-nth lst n old new)
+        (define (atom? x)
+                  (and (not (pair? x)) (not (null? x))))
+
         (define (count-occurences lst old)
                 (cond ((null? lst) 0)
                       ((atom? lst) (if (equal? lst old) 1 0))
                       (else (+ (count-occurences (car lst) old) (count-occurences (cdr lst) old)))))
-)
+
+
+        (define (replace-aux lst n old new count found)
+                  (let ((occ_in_car (count-occurences (cond ((atom? lst) lst)
+                                                            ((null? lst) lst)
+                                                            (else (car lst))) 
+                                                            old)))
+
+                  (cond ((null? lst) '()) 
+                        ((and (atom? lst) (equal? lst old) (equal? count n) (not found)) new)
+                        ((atom? lst) lst)
+
+                        (else (cons 
+                                    (replace-aux (car lst) 
+                                                n 
+                                                old 
+                                                new 
+                                                (if (equal? (car lst) old) (+ count 1) count) 
+                                                (if (equal? n count) #t #f))
+
+                                    (if (> n occ_in_car) 
+                                          (replace-aux (cdr lst) 
+                                                      (- n occ_in_car) 
+                                                      old 
+                                                      new 
+                                                      0 
+                                                      found) 
+                                          (cdr lst)))))))
+
+(replace-aux lst n old new 0 #f))
 
 ; Additional Problems
 
