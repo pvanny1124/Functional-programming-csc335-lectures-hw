6a4e183ed62fba2d602b86bbaa0ccf51f7eae46e
pvanny1124 <pvanny1124@gmail.com>
2018-11-13 18:40:20 -0500

    complete ex 2.32

--- Homework6.scm
+++ Homework6.scm
@@ -63,9 +63,65 @@
 ; Additional Problems
 
 ; Abelson and Sussman, Exercise 2.27 
+
+; Exercise 2.27. Modify your reverse procedure of exercise 2.18 to produce a deep-reverse
+; procedure that takes a list as argument and returns as its value the list with its elements reversed and
+; with all sublists deep-reversed as well. For example,
+
+
+(define (deep-reverse tree)
+            (cond ((null? tree) '())
+                  ((atom? tree) tree)
+                  (else (append (deep-reverse (cdr tree))
+                               (list (deep-reverse (car tree)))))))
+
+;using map
+(define (deep-reverse tree)
+            (cond ((null? tree) '())
+                  ((atom? tree) tree)
+                  (else (reverse (map deep-reverse tree)))))
+
 ; Abelson and Sussman, Exercise 2.29
+
+
 ; Abelson and Sussman, Exercise 2.32
+
+; We can represent a set as a list of distinct elements, and we can represent the set of all
+; subsets of the set as a list of lists. For example, if the set is (1 2 3), then the set of all subsets is
+; (() (3) (2) (2 3) (1) (1 3) (1 2) (1 2 3)). Complete the following definition of a
+; procedure that generates the set of subsets of a set and give a clear explanation of why it works:\
+
+(define (subsets s)
+        (if (null? s)
+            (list '()) 
+             (let ((rest (subsets (cdr s))))
+                 (append rest (map (lambda (seq) (append (list (car s)) seq)) rest)))))
+
+;in every step of the cdr, we are saving the original value of say, (3) as s before () is returned,
+;(2), before (3) is returned and etc from the recursive call to subsets cdr s.
+; once the call is returned, we can simply append the car of s. say (3)
+; to '().
+;in the next step, we would append (2) or the car of (2 3) to (() (3)) to result in ((2) (2 3))
+;which is then appended to (() (3)) to produce (() (3) (2) (2 3))
+; then the program would continue to the original S (1 2 3) and append (1) to each item in (() (3) (2) (2 3))
+; resulting in ((1) (1 3) (1 2) (1 2 3))
+; which is appended to (() (3) (2) (2 3)) resulting in (() (3) (2) (2 3) (1) (1 3) (1 2) (1 2 3))
+
 ; Abelson and Sussman, Exercise 2.37
+
+(define (dot-product v w)
+ (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+ (map <??> m))
+
+(define (transpose mat)
+ (accumulate-n <??> <??> mat))
+
+(define (matrix-*-matrix m n)
+ (let ((cols (transpose n)))
+ (map <??> m)))
+
 ; Abelson and Sussman, Exercise 2.41
 ; Abelson and Sussman, Exercise 2.42
 
